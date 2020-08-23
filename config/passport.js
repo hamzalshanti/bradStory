626d7d55c1168622db3bf1c3ae048e291dad55ac
Hamza Alshanti <hamza2000pal@gmail.com>
2020-08-23 11:14:49 +0300

    new version

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ module.exports = (passport) => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
+        callbackURL: process.env.CALLBACK
     },
     async (accessToken, refreshToken, profile, done) => {
         const newUser = {
